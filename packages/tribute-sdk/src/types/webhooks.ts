eb3c4becd57f7567bf680b514701da3365cab678
Vitaly Gashkov <vitalygashkov@vk.com>
2025-05-29 12:12:37 +0500

    feat(sdk): add webhook event typings

---
+++ packages/tribute-sdk/src/types/webhooks.ts
@@ -0,0 +1,25 @@
+import { Currency } from './currency';
+import { Period } from './subscription-period';
+
+export interface WebhookSubscriptionPayload {
+  subscription_name: string;
+  subscription_id: number;
+  period_id: number;
+  period: Period;
+  price: number;
+  amount: number;
+  currency: Currency;
+  user_id: number;
+  telegram_user_id: number;
+  web_app_link: string;
+  channel_id: number;
+  channel_name: string;
+  expires_at: string;
+}
+
+export interface WebhookEvent {
+  name: string;
+  created_at: string;
+  sent_at: string;
+  payload: WebhookSubscriptionPayload;
+}
